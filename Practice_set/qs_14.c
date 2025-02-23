7d2aef536f8d3a829b8722d604c40040ed994835
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-02-23 19:55:05 +0600

    adding practice sets

---
+++ Practice_set/qs_14.c
@@ -0,0 +1,17 @@
+//print the numbers from 0 to n, if n is given by the user.
+
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    
+    for ( int i = 0; i <n; i++)
+    {
+        printf("%d",i);
+    }
+    
+    return 0;
+}
