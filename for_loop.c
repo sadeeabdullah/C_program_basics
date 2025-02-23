41f22ada2eafee45aa080e24c73311be289fc3c3
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-02-23 14:30:56 +0600

    added practise task on it

--- for_loop.c
+++ for_loop.c
@@ -5,5 +5,20 @@ int main(){
     for(i=0;i<100;i++){
         printf("hello  \n");
     }
+    for(i=10;i>=1;i--){
+        printf("%d\n",i);
+    }
+    //counter
+    int c=0;
+    for ( i = 0; i <=10; i++)
+    {
+        printf("%d\n",i);
+
+        //adding extra counter
+        c++;
+
+    }
     
+    //print the count
+    printf("%d",c);
 }
