215873dfe617e79c9cbc599568cd01a794cad3ac
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-02-25 22:39:59 +0600

    regular commit

--- Practice_set/qs_15.c
+++ Practice_set/qs_15.c
@@ -9,18 +9,19 @@ int main(){
     printf("enter the value of n:");
     scanf("%d",&n);
 
-    for (int i = 1; i <=n; i++)
+    for (int i = 1, j=n; i <=n && j>=1; i++,j--)
     {
         sum=sum+i;
+        printf("%d\n",j);
     }
 
     printf("%d",sum);
 
-    for (int i = n; i >=1; i--)
-    {
-        printf("%d\n",i);
-    }
-    
+    // for (int i = n; i >=1; i--)
+    // {
+    //     printf("%d\n",i);
+    // }
+    printf("regular push");
     return 0;
     
 }
---
+++ do_while_loop.c
@@ -0,0 +1,13 @@
+//first it wil execute once first then check the condition
+
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+    do
+    {
+    printf("%d\n",i);
+    i++;
+    } while (i<=5);
+    
+}
